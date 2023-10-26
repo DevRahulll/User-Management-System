e1b9c14bf079e45ca0e73a90af14fd6aaa71e57f
Rahul <rahuldevan2019@gmail.com>
2023-10-26 21:37:06 +0530

    adding client side code in the repo

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 require('dotenv').config();
-const dbconnect=require('./config/db.js');
-const router=require('./route/user.route.js');
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
+const dbconnect = require('./config/db.js');
+const router = require('./route/user.route.js');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 // database connection
 dbconnect();
 
 
 app.use(cors({
-    origin:"http://localhost:5500",
-    credentials:true
+    origin: "http://localhost:5500",
+    credentials: true
 }))
 
 app.use(cookieParser());
 
 app.use(express.json());
 
-app.use('/',router);
+app.use('/', router);
 
-module.exports=app;
+module.exports = app;
